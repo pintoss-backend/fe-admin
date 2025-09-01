20a59c4b1f36f2a5b66880ca814868e77965fb2b
kwonjeong <kwonjeong9470@gmail.com>
2025-09-01 12:46:11 +0900

    #5 ✨ feat: 회원 명수 컴포넌트 버튼 제거 및 순서 변경

--- src/widgets/UserStats/UserStats.tsx
+++ src/widgets/UserStats/UserStats.tsx
@@ -1,43 +1,23 @@
-import { Row, Col, Card, Statistic, Button, Space } from 'antd';
-import { ReloadOutlined } from '@ant-design/icons';
+import { Card, Row, Col, Statistic } from 'antd';
 
 export const UserStats: React.FC<{
 	total: number;
 	blocked: number;
 	withdrawn: number;
-	onResetList?: () => void;
-}> = ({ total, blocked, withdrawn, onResetList }) => {
+}> = ({ total, blocked, withdrawn }) => {
 	return (
-		<Row gutter={[16, 16]}>
-			<Col xs={24} md={6}>
-				<Card>
-					<Statistic title="총 회원수" value={total} />
-				</Card>
-			</Col>
-			<Col xs={24} md={6}>
-				<Card>
-					<Statistic title="차단 명수" value={blocked} />
-				</Card>
-			</Col>
-			<Col xs={24} md={6}>
-				<Card>
-					<Statistic title="탈퇴 명수" value={withdrawn} />
-				</Card>
-			</Col>
-			<Col xs={24} md={6}>
-				<Card>
-					<Space>
-						<Button
-							icon={<ReloadOutlined />}
-							onClick={() => {
-								onResetList?.();
-							}}
-						>
-							전체 목록
-						</Button>
-					</Space>
-				</Card>
-			</Col>
-		</Row>
+		<Card>
+			<Row gutter={[16, 16]}>
+				<Col xs={24} md={8}>
+					<Statistic title="총 회원수" value={total} suffix="명" />
+				</Col>
+				<Col xs={24} md={8}>
+					<Statistic title="차단 명수" value={blocked} suffix="명" />
+				</Col>
+				<Col xs={24} md={8}>
+					<Statistic title="탈퇴 명수" value={withdrawn} suffix="명" />
+				</Col>
+			</Row>
+		</Card>
 	);
 };
